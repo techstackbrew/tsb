f012ae5cdea93c843766fe21b7c35c40fe0ce3b5
Drew Morris <dhmorris@gmail.com>
2022-11-03 15:17:16 -0400

    forking install script

--- docs/i.sh
+++ docs/i.sh
@@ -1,10 +1,10 @@
 if [[ "$PATH" == *"$HOME/.cargo/bin"* ]]; then
   echo "Installing in $HOME/.cargo/bin..."
-  curl -LSfs https://rossmacarthur.github.io/install/crate.sh | bash -s -- --repo "techstackbrew/tsb" --to ~/.cargo/bin
+  curl -LSfs https://tsbrew.io/crate.sh | bash -s -- --repo "techstackbrew/tsb" --to ~/.cargo/bin
 elif [[ "$PATH" == *"/opt/homebrew/bin"* ]]; then
   echo "Installing in /opt/homebrew/bin..."
-  curl -LSfs https://rossmacarthur.github.io/install/crate.sh | bash -s -- --repo "techstackbrew/tsb" --to /opt/homebrew/bin
+  curl -LSfs https://tsbrew.io/crate.sh | bash -s -- --repo "techstackbrew/tsb" --to /opt/homebrew/bin
 else
   echo "Installing in /usr/local/bin (if this fails you may need to re-run with sudo)..."
-  curl -LSfs https://rossmacarthur.github.io/install/crate.sh | bash -s -- --repo "techstackbrew/tsb" --to /usr/local/bin
+  curl -LSfs https://tsbrew.io/crate.sh | bash -s -- --repo "techstackbrew/tsb" --to /usr/local/bin
 fi
